59b2323e548c6a0a25ae2ec3018b147af2b46e50
shmulik1990 <samuel.schroers@kit.edu>
2022-09-12 14:45:01 +0200

    descriptions added

--- Scenario_example.py
+++ Scenario_example.py
@@ -3,13 +3,22 @@
 Created on Mon Sep 12 11:26:07 2022
 
 @author: shmul
+
+Implementation of a McCormack Scheme for solving 1D Shallow Water Equations,
+including a mass source term for accumulation of rainfall along the flow path.
+
+A description of the scheme can be found in 
+
+- Liang D., Lin B., Flaconer R. A., Simulation of rapidly varying flow using an efficient
+    TVD-MacCormack scheme, int. journal for numerical methods in fluids 2007; 53:811-826 (2007) 
+
 """
 #%% import libraries and functions from 'functions.py'
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 # adjust path to the directory you are woking in...
-path=r'C:\Users\shmul\OneDrive\Documents\KIT_work\DATA_WORK\spyder_bks\swe'
+path=r'C:\Users\...\SWE_example'
 sys.path.append(path)
 import functions as F
 
@@ -24,7 +33,7 @@ x=np.linspace(1,L+1,num=N)
 xp=np.linspace(1-(x[1]-x[0]),L+1+(x[-1]-x[-2]),num=N+2)   # incl. ghost points 
 
 ## vertical
-### create Kirkby Soil Creep and Soil Wash slopes
+### create Kirkby Soil Creep and Soil Wash slopes (compare https://doi.org/10.5194/hess-26-3125-2022)
 xl=xp[-1]-xp[0]
 xf=xp-xp[0]
 YC={}
@@ -53,11 +62,11 @@ Pts[0:int(len(tss)*pend)+1]=Peff
 
 #%% Run code
 
-c=r"C:\Users\shmul\\"
-path_out=c+r"Documents"
+# define the output directory and a name for the run
+path_out=r"C:\Users\\"
 name='test'
 
 t,Q,H=F.SWE(oldrun=False,path_in=path_out,tname="tsave_"+name+"_n",Hname="Hsave_"+name+"_n",Qname="Qsave_"+name+"_n",name=name,x=x,
-                        hini=.0001,tend=1200,ts=5,tss=tss,Pts=Pts,crstart=0.5,dtmax=0.01,
+                        hini=.0001,tend=tend,ts=5,tss=tss,Pts=Pts,crstart=0.5,dtmax=0.01,
                         hgp=YC['Soil Creep'],n=0.1,hmin=0.0001,
                         save=True,path_out=path_out, rst=-99, plotQ=True)
